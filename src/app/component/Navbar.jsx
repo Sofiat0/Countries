55ce18023cac178ed64e3f59746865d550d91fd8
sofiat0 <akinsanyaifeoluwa79@gmail.com>
2023-07-13 16:28:45 -1100

    fix: corect a spelling error

--- src/app/component/Navbar.jsx
+++ src/app/component/Navbar.jsx
@@ -4,33 +4,32 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-    <nav> 
-        <ul className="flex justify-between text-xl capitalize h-[100px] bg-sky-700 text-white items-center">
-   <Link href='/All'>
-   <li>All</li>
-   </Link>
+        <nav>
+            <ul className="flex justify-between text-xl capitalize h-[100px] bg-sky-700 text-white items-center">
+                <Link href='/All'>
+                    <li>All</li>
+                </Link>
 
-   <Link href='/Africa'>
-   <li>Africa</li>
-   </Link>
-   
-   <Link href='/Europe'>
-   <li>Europe</li>
-   </Link>
-   
-   <Link href='/Oceania'>
-   <li>Oceania</li>
-   </Link>
+                <Link href='/Africa'>
+                    <li>Africa</li>
+                </Link>
 
-   <Link href='/Asia'>
-   <li>Asia</li>
-   </Link>
+                <Link href='/Europe'>
+                    <li>Europe</li>
+                </Link>
 
-   <Link href='/Americas'>
-   <li>The Americas</li>
-   </Link>
-     </ul>
-     </nav>
-       )
-  }
-  
+                <Link href='/Oceania'>
+                    <li>Oceania</li>
+                </Link>
+
+                <Link href='/Asia'>
+                    <li>Asia</li>
+                </Link>
+
+                <Link href='/America'>
+                    <li>The Americas</li>
+                </Link>
+            </ul>
+        </nav>
+    )
+}
